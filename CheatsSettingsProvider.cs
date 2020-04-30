8298cf9d988f0797af05acf65e1806a41d05f8bd
Theo <theo.farnole@gmail.com>
2020-04-30 18:26:24 +0200

    Add: [project settings] cheats settings provider

---
+++ CheatsSettingsProvider.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class CheatsSettingsProvider
+{
+    public static readonly string cheatsSettingsPath = "Project/Cheats";
+    public static readonly string cheatsSettingsLabel = "Cheats";
+
+    private static Editor _cheatsSettingsEditor = null;
+
+    private static Editor CheatsSettingsEditor
+    {
+        get
+        {
+            if (_cheatsSettingsEditor == null)
+                CreateCheatsSettingsEditor(out _cheatsSettingsEditor);
+
+            return _cheatsSettingsEditor;
+        }
+    }
+
+
+    [SettingsProvider]
+    public static SettingsProvider CreateCheatsSettingsProvider()
+    {
+        SettingsProvider provider = new SettingsProvider(cheatsSettingsPath, SettingsScope.Project)
+        {
+            label = cheatsSettingsLabel,
+            guiHandler = DrawCheatsSettingsProvider
+        };
+
+        return provider;
+    }
+
+    private static void DrawCheatsSettingsProvider(string searchContext)
+    {        
+        CheatsSettingsEditor.OnInspectorGUI();
+    }
+
+    private static void CreateCheatsSettingsEditor(out Editor editor)
+    {
+        // get cheatsSettings
+        CheatsSettings cheatsSettings = CheatsSettings.GetOrCreateSettings();
+        var serializedObjectCheatsSettings = new SerializedObject(cheatsSettings);
+                
+        editor = Editor.CreateEditor(cheatsSettings);        
+    }
+}
