8df702d3e4f7bb785d60884f798c265c2e40852f
Theo <theo.farnole@gmail.com>
2020-04-30 18:26:02 +0200

    Add: [scriptableObject] cheatsSettings scriptableObject

---
+++ CheatsSettings.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CheatsSettings : ScriptableObject
+{
+    public static readonly string cheatsSettingsPath = "Assets/Editor/CheatsSettings.asset";
+
+    [SerializeField] private Cheat[] _cheats = new Cheat[0];
+
+    public static CheatsSettings GetOrCreateSettings()
+    {
+        CheatsSettings settings = GetSettings();
+
+        if (settings == null)
+            settings = CreateCheatsSettings(out settings, cheatsSettingsPath);
+
+        return settings;
+    }
+
+    static CheatsSettings GetSettings()
+    {
+        return AssetDatabase.LoadAssetAtPath<CheatsSettings>(cheatsSettingsPath);
+    }
+
+    static CheatsSettings CreateCheatsSettings(out CheatsSettings settings, string createAssetPath)
+    {
+        settings = ScriptableObject.CreateInstance<CheatsSettings>();
+        AssetDatabase.CreateAsset(settings, createAssetPath);
+        AssetDatabase.SaveAssets();
+
+        return settings;
+    }
+}
