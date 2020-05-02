b159f0bd1eda09c85642ac1f2b1f0c5bc38c2835
Theo <theo.farnole@gmail.com>
2020-05-02 19:25:31 +0200

    Add: [demo] demo scripts

---
+++ Demo/Scripts/DoubleJump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace TF.Cheats
+{
+    public class DoubleJump : MonoBehaviour
+    {
+        private const string cheatKey = "double_jump";
+        public bool doubleJump;
+
+        void OnEnable()
+        {
+            CheatsManager.OnCheatChanged += CheatsManager_OnCheatUpdated;
+            doubleJump = CheatsManager.GetBool(cheatKey);
+        }
+
+        private void CheatsManager_OnCheatUpdated(string key, CheatType cheatType)
+        {
+            if (key != cheatKey)
+                return;
+            
+            doubleJump = CheatsManager.GetBool(key);
+        }
+    }
+}
---
+++ Demo/Scripts/Invincibility.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TF.Cheats;
+using UnityEngine;
+
+public class Invincibility : MonoBehaviour
+{
+    public bool invincibility;
+
+    private void OnEnable()
+    {
+        CheatsManager.OnCheatChanged += CheatsManager_OnCheatUpdated;
+    }
+
+    void Start()
+    {
+        CheatsManager.SetBool((Cheat)null, false);
+        CheatsManager.SetBool((string)null, false);
+        CheatsManager.SetBool("hfqsdfk", false);
+        CheatsManager.GetBool("hfqsdfk");
+        CheatsManager.GetBool("");
+        CheatsManager.GetBool((Cheat)null);
+        CheatsManager.GetBool((string)null);
+    }
+
+    private void CheatsManager_OnCheatUpdated(string key, CheatType cheatType)
+    {
+        if (key == "invincibility")
+        {
+            invincibility = CheatsManager.GetBool(key);
+        }
+    }
+}
